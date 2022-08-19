e9308fb6e47c71c4557bdc212b6e2f74630a7bbb
vominhtrungpro <vominhtrungpro@gmail.com>
2022-08-19 15:34:12 +0700

    3:34

--- ShopeeFoodDemoBETest/OrderTest.cs
+++ ShopeeFoodDemoBETest/OrderTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
 using ShopeeFoodDemoBE.BLL.Implementations;
 using ShopeeFoodDemoBE.BLL.Models.Dto;
 using ShopeeFoodDemoBE.DAL.EF.Data;
@@ -11,27 +12,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShopeeFoodDemoBETest
+namespace ShopeeFoodDemoBETest 
 {
     public class OrderTest
     {
-        private Mock<IOrderRepository> mockOrderRepository = new Mock<IOrderRepository>();
-        private Mock<DataContext> mockDb = new Mock<DataContext>();
-        [Fact]
-        public async void GetOrderById()
-        {
-            var dtoOrder = new DtoOrder()
-            {
-                OrderId = 1,
-                CustomerId = 1,
-                TimeOrder = Convert.ToDateTime("2022-08-09T02:23:12.927"),
-                PlaceOrder = "Tien Giang",
-                Description = "200000",
-                Status = "Active"
-            };
-            OrderService orderService = new OrderService(mockOrderRepository.Object);
-            var result = await orderService.GetOrderById(1);
-            Assert.Equal(dtoOrder.OrderId, result.OrderId);
-        }
+
     }
 }
